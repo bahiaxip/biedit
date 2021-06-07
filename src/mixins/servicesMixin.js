00934481d031a9a21254335e7cca6f2dfb81b49b
Xip <bahiaxip@hotmail.com>
2021-06-07 20:15:49 +0200

    add cors servicesMixin

--- src/mixins/servicesMixin.js
+++ src/mixins/servicesMixin.js
@@ -646,6 +646,9 @@ export default {
 				
 			};
 			let headers={
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+				"Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
 				headers: {					
 					Authorization: 'Bearer '+api_token
 				}
